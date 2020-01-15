657a10f0d46ebed9da11464cbb9d31444a26f8b6
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2020-01-15 16:11:20 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { Component } from 'react'
+import './App.css'
+
+
+class App extends Component {
+  state: {
+   
+  }
+
+
+  render() {
+    
+      
+    })
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ require('./images/logo.png') } className="App-logo" alt="logo" />
+        </header>
+
+        <Form />
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
